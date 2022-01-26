f9d08a6b483cca52bc6aae6adcbfd4e108f6e815
kailashmandal <hashtagkailash@gmail.com>
2022-01-26 13:08:21 +0530

    added about repl

---
+++ node1.js
@@ -0,0 +1,31 @@
+/**
+ * REPL : Read evalv print loop
+ * 
+ * how to enter ? 
+ * ans : type 'node' and  enter in terimal
+ * 
+ * 
+ * how to exit the Repl ?
+ * ans : type .exit and enter
+ *            OR
+ * ans : crtl+D  from keyword
+ * 
+ * use : we can write Js code in Repl.
+ * 
+ * you can check properties and function of any nuilt in type.
+ * for example :  String. + tab button
+ * some times you have hit twice tab btn.
+ * 
+ */
+/**
+ * how add add a nodejs module in our working file.
+ * well we do require them in our file and storing them into a variable
+ * because require('module') will return a module object.
+ * 
+ * syntax : 
+ *    const <variable-name> = require('<module-name>')
+ */
+
+const fs = require('fs'); // fs is file system module in nodejs
+// fs on left side is storing the fs object returned
+
